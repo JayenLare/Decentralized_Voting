2e7d903ad81d5da761b7f7d21690fc53aa3fbc92
jayenlare <jayenlare@gmail.com>
2023-09-20 15:28:17 -0400

    Added getTime func

--- backend/test/Voting.js
+++ backend/test/Voting.js
@@ -1,5 +1,11 @@
 const { expect } = require("chai");
 
+const getTime = async () => {
+  const blockNumBefore = await ethers.provider.getBlockNumber();
+  const blockBefore = await ethers.provider.getBlock(blockNumBefore);
+  return blockBefore.timestamp;
+}
+
 describe("Voting", function () {
   let addr0;
   let addr1;
