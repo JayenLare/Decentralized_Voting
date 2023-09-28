6f46a04d5d07ba16f2885ea2eba9f637883601f7
jayenlare <jayenlare@gmail.com>
2023-09-27 21:00:37 -0400

    Set up routes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,13 @@
 import "bootstrap/dist/css/bootstrap.min.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
 
 function App() {
-  return (
-    <div className="App">
-      
-    </div>
-  );
+  return <Router>
+    <Routes>
+      <Route path = "create-vote" element = {<></>} />
+      <Route path = "votes" element = {<></>} />
+    </Routes>
+  </Router>
 }
 
 export default App;
